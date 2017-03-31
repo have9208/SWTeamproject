8d1dbbd39cdfcaa18afb9dbe128deabb82108478
DESKTOP-2HED1KJ\com <codingflowerdog@gmail.com>
2017-03-31 09:41:02 +0900

    YUN TAEHEE Commit

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv) {
 	printf("hello SWTeamproject !!\n");
-	printf("commit man");
+	printf("YUN TAEHEE Commit!!\n");
 	printf("Gyeong-su Han Commit!!\n");
 	return 0;
 }
