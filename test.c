6b960ad642052342a632ea2fb44cf7ad910c3412
DESKTOP-2HED1KJ\com <rsh0421@gmail.com>
2017-03-31 09:46:48 +0900

    SeungHyun Lee commit

--- test.c
+++ test.c
@@ -4,5 +4,6 @@ int main(int argc, char **argv) {
 	printf("hello SWTeamproject !!\n");
 	printf("YUN TAEHEE Commit!!\n");
 	printf("Gyeong-su Han Commit!!\n");
+	printf("Seung-Hyun Lee Commit!!\n");
 	return 0;
 }
