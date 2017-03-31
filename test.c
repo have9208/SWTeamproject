fd67cf02cdd6a3e768c0fde5c9900d30a95b0929
Micalgenus <micalgenus@gmail.com>
2017-03-31 09:37:42 +0900

    gyeongsu han commit
    hello!!

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char **argv) {
 	printf("hello SWTeamproject !!\n");
-	printf("commit man");  								
+	printf("commit man");
+	printf("Gyeong-su Han Commit!!\n");
 	return 0;
 }
