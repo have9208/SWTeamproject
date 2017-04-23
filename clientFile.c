1cf58c6ced924505ccd63d329b970adc85f0cf67
OMG <plusbin4@naver.com>
2017-04-24 05:21:43 +0900

    파일 무결성검사 완료

--- clientFile.c
+++ clientFile.c
@@ -1,4 +1,59 @@
 #include "clientFile.h"
+
+DataFile* readFile(char *fileName)
+{
+    int fd;
+    char buf[BUFF_SIZE];
+    DataFile *fileBuf;
+    SHA256_CTX ctx;
+    
+    fileBuf = (DataFile*)malloc(sizeof(DataFile));
+    memset(fileBuf,0,sizeof(DataFile));
+    
+    fd = openFile(fileName);
+    
+    fileBuf->fileSize = getFileSize(fd);
+    
+    ctx = gethash(fd,ctx,&fileBuf);
+    sha256_final(&ctx,fileBuf->hash);
+    
+    fileBuf->file = (char*)malloc(sizeof(char)*fileBuf->fileSize);
+    memset(fileBuf->file,0,sizeof(char)*fileBuf->fileSize);
+    
+    lseek(fd,0,SEEK_SET);
+    
+    if(read(fd, fileBuf->file,fileBuf->fileSize) == -1)
+    {
+        printf("read() error \n");
+    }
+    
+    close(fd);
+    
+    return fileBuf;
+}
+SHA256_CTX gethash(int fd,SHA256_CTX ctx,DataFile *fileBuf)
+{
+    int ret;
+    
+    lseek(fd,0,SEEK_SET);
+    sha256_init(&ctx);
+    char buf[BUFF_SIZE];
+    while(1)
+    {
+        ret=read(fd, buf, BUFF_SIZE);
+        if(ret > 0)
+        {
+            sha256_update(&ctx,buf,ret);
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    return ctx;
+}
+
 int openFile(char *fileName)
 {
     int fd;
@@ -30,31 +85,4 @@ int getFileSize(int fd)
         }
     }
     return size;
-}
-DataFile* readFile(char *fileName)
-{
-    int fd;
-    char buf[BUFF_SIZE];
-    DataFile *fileBuf;
-    
-    fileBuf = (DataFile*)malloc(sizeof(DataFile));
-    memset(fileBuf,0,sizeof(DataFile));
-    
-    fd = openFile(fileName);
-    
-    fileBuf->fileSize = getFileSize(fd);
-    
-    
-    fileBuf->file = (char*)malloc(sizeof(char)*fileBuf->fileSize);
-    memset(fileBuf->file,0,sizeof(char)*fileBuf->fileSize);
-    
-    lseek(fd,0,SEEK_SET);
-    
-    if(read(fd, fileBuf->file,fileBuf->fileSize) == -1)
-    {
-        printf("read() error \n");
-    }
-    close(fileBuf->file);
-    close(fileBuf);
-    return fileBuf;
 }
--- clientFile.h
+++ clientFile.h
@@ -7,17 +7,21 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include "sha256.h"
 
 #define BUFF_SIZE 1024
+#define HASH_SIZE 32
 
 typedef struct DataFile
 {
     char *file;
     int fileSize;
+    unsigned char hash[HASH_SIZE];
 } DataFile;
 
 int getFileSize(int fd);
 int openFile(char *fileName);
 DataFile* readFile();
+SHA256_CTX gethash(int fd,SHA256_CTX crt,DataFile *fileBuf);
 
 #endif
