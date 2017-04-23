6fe4c3982fb6533f1e4753ae533c072dd74b1dd0
codingflowerdog <slboxer@naver.com>
2017-04-24 01:20:24 +0900

    serverFile 파일 중복 문제

--- serverFile.h
+++ serverFile.h
@@ -1,6 +1,14 @@
 #ifndef __SERVER_FILE_H__
 #define __SERVER_FILE_H__
 
+#include "file.h"
+#include <stdio.h>         // puts()
+#include <string.h>        // strlen()
+#include <fcntl.h>         // O_WRONLY
+#include <unistd.h>        // write(), close()
+#include <sys/types.h>
+#include <sys/stat.h>
+
 void createFile(char *fileBuffer,char *fileName, int fileSize);
 int writeFile(char *fileBuffer,char *fileName, int fileSize);
 char *writeError();
